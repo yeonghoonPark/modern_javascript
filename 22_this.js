02ee69c923dcb7917e84edca8e6826dc33075b88
yeonghoonPark <huklmnj2@naver.com>
2025-01-27 12:38:11 +0900

    22. this (22-2-2. 메서드 호출)

--- 22_this.js
+++ 22_this.js
@@ -8,7 +8,7 @@
  * 2. 객체 안에서 호출하는 경우, 객체 자신을 가리킨다.
  * 3. constructor 생성자 함수에서 호출하는 경우, 미래에 생성될 인스턴스를 가리킨다.
  * 4. 일반 함수에서 호출하는 경우, 동적으로 `this`가 호출되는 위치에 따라 달라진다. (`use strict` 사용 시 `undefined`, 아닐 경우 `window` 또는 `global`)
- * 5. non-constructor 함수에서 호출하는 경우, 정적으로 상위 스코프의 `this`를 가리킨다. (non-constructor인 ES6 메서드 축약은 객체 내부에서 호출되기 때문에 객체 자신을 가리킨다.)
+ * 5. non-constructor 함수인 화살표 함수에서 호출하는 경우, 정적으로 상위 스코프의 `this`를 가리킨다. (non-constructor인 ES6 메서드 축약은 객체 내부에서 호출되기 때문에 객체 자신을 가리킨다.)
  *
  * `this`는 객체의 프로퍼티나 메서드를 참조하기 위한 자기 참조 변수이다.
  * 즉, 객체의 메서드나 생성자 함수에서만 의미가 있기 때문에 `use strict`에서는 일반 함수의 경우 `undefined`가 바인딩된다.
@@ -51,7 +51,7 @@ const bar = () => {
 // 객체 내부의 화살표 함수에서 `this`를 호출하면 객체 자신이 아닌 상위 스코프를 가리킨다.
 const animal = {
   name: "Tiger",
-  getName: () => this.name, // undefined, (상위 스코프의 `this`, 즉 `window`의 프로퍼티에서 name을 찾기 때문이다.)
+  getName: () => this.name, // '', (상위 스코프의 `this`, 즉 `window`의 프로퍼티에서 name은 브라우저 창의 이름을 가리키며 기본값을 '' 이다.)
 };
 
 // 객체 내부에서 ES6 축약 메서드에서 `this`를 호출하면 객체 자신을 가리킨다.
@@ -110,7 +110,7 @@ const insect = {
   arrowFunc.bind(student)(); // window
 }
 
-// 22-2-1. 일반 함수 호출
+// 1️⃣ 22-2-1. 일반 함수 호출
 // 👉 기본적으로 중첩 함수까지 모든 `this`에는 전역 객체가 바인딩된다. (단 strict 모드에서는 `undefined`가 바인딩된다)
 function qux() {
   // 'use strict'
@@ -125,7 +125,7 @@ function qux() {
 
 qux();
 
-// ❗️ 메서드 내에서 정의한 중첩 함수가 일반 함수로 호출된다면 `this`는 전역 객체가 바인딩 된다.
+// ❗️ 메서드 내에서 정의한 중첩 함수 또는 콜백 함수가 일반 함수로 호출된다면 `this`는 전역 객체가 바인딩 된다.
 // 전역 변수
 var value = 1;
 
@@ -172,13 +172,13 @@ var value = 1;
         console.log(that.value); // 100
       }
 
-      // 메서드 내에서 중첩 함수를 화살표 함수로 호출하여 `this`가 상위 스코프(`obj`)의 `this`를 참조하도록 한다.
+      // 메서드 내에서 중첩 함수를 화살표 함수로 호출하여 `this`가 정적으로 상위 스코프(`obj`)의 `this`를 참조하도록 한다.
       const getThis = () => {
         console.log(this); // obj
         console.log(this.value); // 100
       };
 
-      // 메서드 내에서 콜백 함수를 화살표 함수로 호출하여 `this`가 상위 스코프(`obj`) `this`를 참조하도록 한다.
+      // 메서드 내에서 콜백 함수를 화살표 함수로 호출하여 `this`가 정적으로 상위 스코프(`obj`) `this`를 참조하도록 한다.
       setTimeout(() => {
         console.log(this); // obj
         console.log(this.value); // 100
@@ -204,3 +204,58 @@ var value = 1;
 // strict 모드에서 `undefined`를 참조하도록 하는 이유가 있다. (예상치 못한 오류나 혼란을 방지하는 목적)
 // 코드를 트랙킹 하는 입장에서 메서드 내의 중첩 함수나 콜백 함수를 일반 함수로 호출하게 되면 `this`는 해당 객체가 아닌 전역 객체를 참조하기 때문에 많은 혼란을 준다.
 // `this`는 일반 함수에서는 사용하지 말고, 명확하게 필요로 하는 곳(객체, 생성자 함수, 클래스)에서만 사용해야 한다.
+
+// 2️⃣ 22-2-2. 메서드 호출
+// 👉 메서드 내부의 `this`는 메서드를 호출한 객체, 즉 호출한 객체를 참조한다.
+// ❗️ 주의할 점은 메서드 내부의 `this`는 메서드를 소유한 객체가 아닌 메서드를 호출한 객체를 참조한다는 점이다.
+{
+  const person = {
+    name: "Jeff",
+    getName() {
+      // 메서드 내부의 `this`는 `this`를 소유한 `getName`이 아닌 호출한 객체를 참조한다.
+      return this.name;
+    },
+  };
+
+  // `getName`을 호출한 객체는 `person`으로 `this`는 `person`을 참조한다.
+  person.getName(); // Jeff
+
+  // 위의 `person`객체의 `getName`은 객체에 바인딩된 함수다.
+  // 즉, `person`객체의 `getName`프로퍼티가 가리키는 함수 객체는 `person`객체에 포함된 것이 아닌 독립적인 별도의 객체다.
+  // `getName`프로퍼티가 독립적인 함수 객체를 참조하고 있을 뿐이다.
+  // 따라서 `getName`프로퍼티가 가리키는 독립적인 함수 객체는 다른 객체의 프로퍼티로도 활용할 수 있다.
+
+  const anotherPerson = {
+    name: "Mark",
+  };
+
+  // `anotherPerson`객체에 `getName`프로퍼티 키를 추가하여 참조값을 `person`의 `getName`이 참조하는 독립적인 함수로 할당
+  anotherPerson.getName = person.getName;
+  anotherPerson.getName(); // Mark
+
+  // `person`의 `getName`이 참조하는 독립적인 함수를 변수에 할당
+  const getName = person.getName;
+  getName(); // '', `this`는 window를 가리킨다. (브라우저에서 window.name은 브라우저 창의 이름을 나타내는 프로퍼티이며 기본값은 '' 이다.)
+
+  // 프로토타입 메서드 내부에서 사용된 `this`도 일반 메서드와 마찬가지로 해당 메서드를 호출한 객체를 참조한다.
+  function Person(name) {
+    this.name = name;
+  }
+
+  Person.prototype.getName = function () {
+    return this.name;
+  };
+
+  const julian = new Person("Julian");
+
+  // `getName`메서드를 호출한 객체는 `julian`이다.
+  julian.getName(); // Julian
+
+  Person.prototype.name = "Williams";
+
+  // `getName`메서드를 호출한 객체는 `Person.prototype`이다.
+  Person.prototype.getName(); // Williams
+
+  // 🔑 `this`를 메서드 내부에서 사용하면 `this`를 소유하는 객체가 아닌 메서드를 호출한 객체를 참조한다.
+  // 즉 메서드 내부에서 `this`는 호출 시점에 따라 참조할 객체가 정해진다.
+}
