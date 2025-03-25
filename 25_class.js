442183ac9e7582ea92a3890eacd255e7027841c1
yeonghoonPark <huklmnj2@naver.com>
2025-03-25 20:04:06 +0900

    25. class (25-2. 클래스 정의)

--- 25_class.js
+++ 25_class.js
@@ -18,3 +18,53 @@
  * `class`는 생성자 함수보다 강력한 프로토타입 기반의 문법적 설탕이라고 볼 수도 있지만, 새로운 객체 생성 메커니즘으로 보는게 더 합당하다.
  *
  */
+
+/**
+ * 25-2. 클래스 정의
+ *
+ * 클래스는 `class` 키워드를 사용하여 정의한다.
+ * 클래스는 익명 클래스 표현식과 기명 클래스 표현식으로도 사용할 수 있다.
+ * 이 말은, 클래스는 값으로 사용할 수 있는 일급 객체임을 의미한다.
+ *
+ * 일급 객체는 다음과 같은 특징을 갖는다.
+ * 1. 무명의 리터럴로 생성할 수 있다. 즉, 런타임에 생성이 가능하다.
+ * 2. 변수나 자료구조(객체, 배열 등)에 저장할 수 있다.
+ * 3. 함수의 매개변수에 전달할 수 있다.
+ * 4. 함수의 반환값으로 사용할 수 있다.
+ * 즉, 클래스는 함수이다. 따라서 클래스는 값처럼 사용할 수 있는 일급 객체다.
+ *
+ * 클래스 몸체에 정의할 수 있는 메서드는 세 가지가 있다.
+ * 1. constructor(생성자)
+ * 2. 프로토타입 메서드
+ * 3. 정적 메서드
+ *
+ */
+
+{
+  class Person {
+    // constructor
+    constructor(name) {
+      // create instance and initialize
+      this.name = name;
+    }
+
+    // prototype method
+    sayHi() {
+      console.log(`Hi, My name is ${this.name}`);
+    }
+
+    // static method
+    static sayHello() {
+      console.log("Hello!");
+    }
+  }
+
+  // instance
+  const john = new Person("John");
+
+  // call prototype method
+  john.sayHi(); // Hi, My name is John
+
+  // call static method
+  Person.sayHello(); // Hello!
+}
